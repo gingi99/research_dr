ec3a793457454994710f393ad5b519386958ec46
gingi99 <bigtree3101@gmail.com>
2015-10-25 16:18:05 +0900

    バグ取り

--- rule_clustering/RuleClustering.R
+++ rule_clustering/RuleClustering.R
@@ -42,8 +42,8 @@ cal_dist_rules <- function(df){
 
 ## 2つのルールをマージして新しいルールを作る
 mergeRules <- function(rule1, rule2){
-  print(rule1)
-  print(rule2)
+  #print(rule1)
+  #print(rule2)
   
   # 2つのルールに両方持つ条件属性集合を抽出
   idxs <- intersect(rule1$idx, rule2$idx)
@@ -78,7 +78,7 @@ mergeRules <- function(rule1, rule2){
   if(rule1$consequent == rule2$consequent){
     consequent.new <- rule1$consequent
   }else{
-    consequent.new <- paste("[",rule1$consequent,",",rule2$consequent,"]",sep="")
+    consequent.new <- c(rule1$consequent,rule2$consequent)
   }
   # support数の計算
   suport.new <- sort(union(rule1$support, rule2$support))
@@ -94,6 +94,8 @@ mergeRules <- function(rule1, rule2){
 
 ## ルール間の距離から、ルールを併合して新しいルールを作る
 recreate_rules_by_dist <- function(mat.df.dist, rules, k=3){
+  # 初期化
+  rules.new <- rules 
   for(di in 1:length(rules)){
     mat.df.dist[di, di] <- Inf
   }
@@ -110,50 +112,73 @@ recreate_rules_by_dist <- function(mat.df.dist, rules, k=3){
       ind.merge.target.rule <- ind.minValue[1]
     }
     vec.dist <- mat.df.dist[ind.merge.target.rule,] 
-    minValue <- min(vec.dist)
+    minDistValue <- min(vec.dist)
     # 最も距離が近いルールを求める
-    ind.minValue <- which(vec.dist == minValue)
-    if(length(ind.minValue) == 1){
-      rule.new <- mergeRules(rules[[ind.merge.target.rule]], rules[[ind.minValue]])
+    ind.minDistValue <- which(vec.dist == minDistValue)
+    if(length(ind.minDistValue) == 1){
+      ind.merge.target.rule2 <- ind.minDistValue
+      rule.new <- mergeRules(rules[[ind.merge.target.rule]], rules[[ind.merge.target.rule2]])
     }else{
       # 結論部が同じルールかどうか
-      vec.consequent <- sapply(ind.minValue, function(x){
+      vec.consequent <- sapply(ind.minDistValue, function(x){
         return(rules[[x]]$consequent)
       })
-      ind.minValue <- ind.minValue[vec.consequent == rules[[ind.merge.target.rule]]$consequent]
-      if(length(ind.minValue) == 1){
-        rule.new <- mergeRules(rules[[ind.merge.target.rule]], rules[[ind.minValue]])
+      ind.minConsequentValue <- ind.minDistValue[vec.consequent == rules[[ind.merge.target.rule]]$consequent]
+      # 結論部が同じルールが1つ
+      if(length(ind.minConsequentValue) == 1){
+        ind.merge.target.rule2 <- ind.minConsequentValue
+        rule.new <- mergeRules(rules[[ind.merge.target.rule]], rules[[ind.merge.target.rule2]])
+      # 結論部が同じルールが2つ以上 or ない
       }else{
+        # ないなら、距離が小さいルールを再度入れなおす
+        if(length(ind.minConsequentValue) == 0){
+          ind.minConsequentValue <- ind.minDistValue
+        }
         # 条件部を構成する属性名がなるべく同じであるか
-        vec.match.length.idx <- sapply(ind.minValue, function(x){
+        vec.match.length.idx <- sapply(ind.minConsequentValue, function(x){
           return(length(intersect(rules[[x]]$idx, rules[[ind.merge.target.rule]]$idx)))
         })
-        minValue <- min(vec.match.length.idx)
-        ind.minValue <- ind.minValue[vec.match.length.idx == minValue]
-        if(length(ind.minValue) == 1){
-          rule.new <- mergeRules(rules[[ind.merge.target.rule]], rules[[ind.minValue]])
+        maxConditionValue <- max(vec.match.length.idx)
+        ind.maxConditionValue <- ind.minConsequentValue[vec.match.length.idx == maxConditionValue]
+        if(length(ind.maxConditionValue) == 1){
+          ind.merge.target.rule2 <- ind.maxConditionValue
+          rule.new <- mergeRules(rules[[ind.merge.target.rule]], rules[[ind.merge.target.rule2]])
         }else{
           # support sizeが小さいルールにマージする
-          minValue <- min(vec.supportsize[ind.minValue])
-          ind.minValue <- ind.minValue[which(vec.supportsize[ind.minValue] == minValue)]
-          if(length(ind.minValue) == 1){
-            rule.new <- mergeRules(rules[[ind.merge.target.rule]], rules[[ind.minValue]])
+          minSupportValue <- min(vec.supportsize[ind.maxConditionValue])
+          ind.minSupportValue <- ind.maxConditionValue[which(vec.supportsize[ind.maxConditionValue] == minSupportValue)]
+          if(length(ind.minSupportValue) == 1){
+            ind.merge.target.rule2 <- ind.minSupportValue
+            rule.new <- mergeRules(rules[[ind.merge.target.rule]], rules[[ind.merge.target.rule2]])
           }else{
             # なければ、候補ルールの中の最初のルールとマージする
-            ind.minValue <- ind.minValue[1]
-            rule.new <- mergeRules(rules[[ind.merge.target.rule]], rules[[ind.minValue]])
+            ind.merge.target.rule2 <- ind.minSupportValue[1]
+            rule.new <- mergeRules(rules[[ind.merge.target.rule]], rules[[ind.merge.target.rule2]])
           }
         }
       }
     }
-    rules <- rules[-c(ind.merge.target.rule,ind.minValue)]
-    rules <- list.append(rules, rule.new)
-    vec.supportsize <- sapply(rules, function(rule){
+    rules.new <- rules.new[-c(ind.merge.target.rule, ind.merge.target.rule2)]
+    rules.new <- list.append(rules.new, rule.new)
+    vec.supportsize <- sapply(rules.new, function(rule){
       return(length(rule$support))
     })
+    print(vec.supportsize)
     minSupportValue <- min(vec.supportsize)
   }
-  return(rules)
+  
+  # attributeをつける
+  attr(rules.new, "uniqueCls") <- attributes(rules)$uniqueCls
+  attr(rules.new, "clsProbs") <- attributes(rules)$clsProbs
+  attr(rules.new, "majorityCls") <- attributes(rules)$majorityCls
+  attr(rules.new, "method") <- "MLEM2Rules"
+  attr(rules.new, "dec.attr") <- attributes(rules)$dec.attr
+  attr(rules.new, "colnames") <- attributes(rules)$colnames
+  
+  # RuleSetRSTクラスを付与し、rulesの記述を指定フォーマットに変える
+  source("~/R/roughsets/My.ObjectFactory.R")
+  rules.new <- My.ObjectFactory(rules.new, classname = "RuleSetRST")
+  return(rules.new)
 }
 
 # main 関数

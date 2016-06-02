799c51759041413b3ca9001611f17d73bf044fd0
m.ooki <m.ooki@nttv6.jp>
2016-06-03 08:45:04 +0900

    stats 追加

--- R/stats/stats.R
+++ R/stats/stats.R
@@ -15,7 +15,7 @@ library(Hmisc)
 # データ読み込み
 # ===========================================
 
-df <- read_csv("../../python/Experiment/hayes-roth.log",col_names = F)
+df <- read_csv("../python/Experiment/hayes-roth2.log",col_names = F)
 
 # ===========================================
 # データクレンジング
@@ -41,8 +41,37 @@ df %>%
   mutate(k = as.character(k)) %>%
   ggplot(aes(x=k, y=acc, color=method)) +
     geom_boxplot() +
-    facet_grid(filename~.)
+    facet_grid(filename~.) +
+    theme(legend.position = "bottom")
 
+# acc の 平均線
+df %>%
+  filter(method == "MLEM2_LERS" | 
+           method == "MLEM2_OnlyK_LERS" | 
+           method == "MLEM2_RuleClusteringBySim_LERS" | 
+           method == "MLEM2_RuleClusteringByRandom_LERS" |
+           method == "MLEM2_RuleClusteringBySameCondition_LERS" |
+           method == "MLEM2_RuleClusteringByConsistentSim_LERS" |
+           method == "MLEM2_RuleClusteringByConsistentSimExceptMRule_LERS") %>%
+  mutate(k = formatC(.$k, width=2, flag="0")) %>%
+  mutate(k = as.character(k)) %>%
+  group_by(filename, k, method) %>%
+  summarise(mean_acc = mean(acc,na.rm=T), 
+            sd_acc = sd(acc,na.rm=T)) %>% 
+  ggplot(aes(x=k, y=mean_acc, group = method, color=method)) +
+    geom_line() +
+    geom_point(size=3, shape = 21) +
+    #geom_text(aes(x="01", y=mean_acc, label=method))
+    facet_grid(filename~.) +
+    scale_color_brewer(palette = "Set1") +
+    labs(x="k", y="正答率の平均値") +
+    theme_bw(base_family = "HiraKakuProN-W3") +
+    theme(axis.title.x = element_text(size=15)) +
+    theme(axis.text.x = element_text(size=12)) +
+    theme(axis.title.y = element_text(size=15)) +
+    theme(axis.text.y = element_text(size=12)) +
+    theme(legend.position = "bottom")
+  
 
 # ===========================================
 # latex 形式の表

526cbc4814e2d0ad7696fd61e2589018e13ae5a4
m.ooki <m.ooki@nttv6.jp>
2016-06-07 18:32:25 +0900

    FILENAMEのログファイルができるように

--- python/Experiment/ExperimentsMLEM2.py
+++ python/Experiment/ExperimentsMLEM2.py
@@ -418,13 +418,13 @@ if __name__ == "__main__":
     #    print(MLEM2_LERS(FILENAME, iter1, iter2))
 
     # 実行したい実験関数
-    FUN = MLEM2_LERS
+    #FUN = MLEM2_LERS
     #FUN = MLEM2_OnlyK_LERS
     #FUN = MLEM2_RuleClusteringBySim_LERS
     #FUN = MLEM2_RuleClusteringByRandom_LERS
     #FUN = MLEM2_RuleClusteringBySameCondition_LERS
     #FUN = MLEM2_RuleClusteringByConsistentSim_LERS
-    #FUN = MLEM2_RuleClusteringByConsistentSimExceptMRule_LERS
+    FUN = MLEM2_RuleClusteringByConsistentSimExceptMRule_LERS
 
     #FUNS = [MLEM2_LERS,
     #        MLEM2_OnlyK_LERS,
@@ -435,7 +435,7 @@ if __name__ == "__main__":
     #        MLEM2_RuleClusteringByConsistentSimExceptMRule_LERS]
 
     # 並列実行
-    proc=4
+    proc=48
     freeze_support()
     
     #for FUN in FUNS :

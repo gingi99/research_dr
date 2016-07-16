109e7754794e3496f678f64f2263f3a9fd312966
gingi99 <bigtree3101@gmail.com>
2016-07-16 21:35:53 +0900

    データの属性値をアルファベット形式で出力するスクリプト

---
+++ R/createData/createAlphatbetNumberData.R
@@ -0,0 +1,41 @@
+# 実験用にa1,a2,...D1,D2,D3のようにデータを作る
+
+# ===========================================
+# ライブラリ読み込み
+# ===========================================
+library(dplyr)
+library(readr)
+library(rlist)
+library(data.table)
+library(stringr)
+library(tidyr)
+
+FILENAME <- "hayes-roth"
+DIRPATH <- paste0("/data/uci/",FILENAME)
+files.all <- list.files(DIRPATH)
+files.train <- files.all[str_detect(files.all, "train[0-9]")]
+files.test <- files.all[str_detect(files.all, "test")]
+files.target <- c(files.train, files.test)
+
+# 各ファイルごとにdfとしてロードして処理
+for(f in files.target){
+  # 読み込み
+  df <- read_tsv(paste0(DIRPATH,"/",f), col_names = T)
+  df <- na.omit(df)
+  
+  # カラム名を定義
+  colnames_new <- c(letters[1:ncol(df)-1],"D")
+  setnames(df, colnames_new)
+  
+  # 属性値を変更する
+  df.alpha <- df
+  for(i in seq(1,ncol(df))){
+    new_vals <- paste0(colnames_new[i], seq(1,length(unique(df[[i]]))))
+    df.alpha[[i]] <- factor(df[[i]], levels = sort(unique(df[[i]])), labels = new_vals)
+  }
+  
+  # 保存
+  write.table(df.alpha, 
+              paste0("/data/uci/",FILENAME,"/alpha/",f), 
+              sep = " ", row.names=F, quote=F, col.names = F)
+}
